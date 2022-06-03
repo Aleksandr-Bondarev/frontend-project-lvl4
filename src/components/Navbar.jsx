1c5838b0446cea905c6024631144ac99717562da
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-06-03 22:56:39 +0300

    added button to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-// <button type="button" className="btn btn-primary">Выйти</button>
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext.jsx';
+import { useNavigate } from 'react-router-dom';
+
 function Navbar() {
+  const { toLogOut, isAuthorized } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const clickButton = () => {
+    toLogOut();
+    navigate('/');
+  };
+
   return (
     <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
       <div className="container">
         <a className="navbar-brand" href="/">Hexlet Chat</a>
+        { isAuthorized() && <button
+        type="button" className="btn btn-primary" onClick={clickButton}>Выйти</button> }
       </div>
     </nav>
   );
