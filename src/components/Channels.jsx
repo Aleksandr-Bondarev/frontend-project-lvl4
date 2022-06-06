231f775a95cb30bc1ed06cc4abff51e766c27222
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-06-06 19:52:11 +0300

    installed classnames and updated channels render

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import classNames from 'classnames';
 
 const Channels = () => {
   const chatChannels = useSelector((state) => state.channels.channels);
 
   const channelsList = chatChannels.map((channel) => (
     <li className="nav-item w-100" key={channel.id}>
-      <button type="button" className="w-100 rounded-0 text-start btn">
+      <button type="button" className={classNames('w-100', 'rounded-0', 'text-start', 'btn', { 'btn-secondary': channel.name === 'general' })}>
         <span className="me-1">#</span>
         {channel.name}
       </button>
