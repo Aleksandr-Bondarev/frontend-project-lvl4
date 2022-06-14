dd8f3d19afc169157d4c86c261a72e076a731692
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-06-14 16:14:57 +0300

    signUp validation and handle error 409

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,12 +1,32 @@
 /* eslint jsx-a11y/label-has-associated-control: [0] */
+/* eslint jsx-a11y/no-autofocus: [0] */
 
 import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
+import classNames from 'classnames';
 import signUp from '../images/signUp.jpeg';
 import { AuthContext } from '../context/AuthContext.jsx';
+
+const handleConflict = () => {
+  const usernameInput = document.querySelector('#username');
+  const passwordInput = document.querySelector('#password');
+  const confirmInput = document.querySelector('#confirmPassword');
+
+  usernameInput.classList.add('is-invalid');
+  passwordInput.classList.add('is-invalid');
+  confirmInput.classList.add('is-invalid');
+
+  const container = document.getElementById('confirmPassword').parentNode;
+
+  const tooltipConfirm = document.createElement('div');
+  tooltipConfirm.classList.add('invalid-tooltip');
+  tooltipConfirm.textContent = 'Такой пользователь уже существует';
+
+  container.appendChild(tooltipConfirm);
+};
 
 function SignUp() {
   const { toLogIn } = useContext(AuthContext);
@@ -20,41 +40,29 @@ function SignUp() {
     },
 
     validationSchema: yup.object({
-      username: yup.string().required('Username is required'),
-      password: yup.string().required('Password is required'),
+      username: yup.string()
+        .required('Обязательное поле')
+        .min(3, 'От 3 до 20 символов')
+        .max(20, 'От 3 до 20 символов'),
+      password: yup.string()
+        .required('Обязательное поле')
+        .min(6, 'Не менее 6 символов'),
       confirmPassword: yup.string()
-        .oneOf([yup.ref('password'), null], 'Passwords must match'),
+        .oneOf([yup.ref('password'), null], 'Пароли должны совпадать')
+        .required('Обязательное поле'),
     }),
 
     onSubmit: async ({ username, password }) => {
-      // console.log({ username, password });
-      // console.log('first logging');
       try {
-        // console.log('pre post logging');
         const response = await axios.post('api/v1/signup', {
           username,
           password,
         });
         toLogIn(response.data);
-        // console.log(response);
-        // console.log('log in try');
-        // console.log(response.data.token)
         navigate('/');
       } catch (e) {
-        // console.log(e);
-        // console.log(e.response.data.message)
         if (e.response.data.message === 'Conflict') {
-          const usernameInput = document.querySelector('#username');
-          const passwordInput = document.querySelector('#password');
-          const confirmInput = document.querySelector('#confirmPassword');
-
-          usernameInput.classList.add('is-invalid');
-          passwordInput.classList.add('is-invalid');
-          confirmInput.classList.add('is-invalid');
-
-          const tooltipConfirm = document.querySelector('#tooltipConfirmPassword');
-
-          tooltipConfirm.textContent = 'Такой пользователь уже существует';
+          handleConflict();
         }
       }
     },
@@ -79,16 +87,20 @@ function SignUp() {
                 <h1 className="text-center mb-4">Регистрация</h1>
                 <div className="form-floating mb-3">
                   <input
+                    autoFocus
                     placeholder="От 3 до 20 символов"
                     name="username"
                     autoComplete="username"
                     required=""
                     id="username"
-                    className="form-control"
+                    className={classNames('form-control', { 'is-invalid': (formik.errors.username && formik.touched.username) })}
                     value={formik.values.username}
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                   />
-                  <div className="invalid-tooltip" id="tooltipUsername" />
+                  {formik.errors.username && formik.touched.username ? (
+                    <div className="invalid-tooltip" id="tooltipUsername">{formik.errors.username}</div>
+                  ) : null}
                   <label className="form-label" htmlFor="username">Имя пользователя</label>
                 </div>
                 <div className="form-floating mb-3">
@@ -100,11 +112,14 @@ function SignUp() {
                     autoComplete="new-password"
                     type="password"
                     id="password"
-                    className="form-control"
+                    className={classNames('form-control', { 'is-invalid': (formik.errors.password && formik.touched.password) })}
                     value={formik.values.password}
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                   />
-                  <div className="invalid-tooltip" id="tooltipPassword" />
+                  {formik.errors.password && formik.touched.password ? (
+                    <div className="invalid-tooltip" id="tooltipPassword">{formik.errors.password}</div>
+                  ) : null}
                   <label className="form-label" htmlFor="password">Пароль</label>
                 </div>
                 <div className="form-floating mb-4">
@@ -115,11 +130,14 @@ function SignUp() {
                     autoComplete="new-password"
                     type="password"
                     id="confirmPassword"
-                    className="form-control"
+                    className={classNames('form-control', { 'is-invalid': (formik.errors.confirmPassword && formik.touched.confirmPassword) })}
                     value={formik.values.confirmPassword}
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                   />
-                  <div className="invalid-tooltip" id="tooltipConfirmPassword" />
+                  {formik.errors.confirmPassword && formik.touched.confirmPassword ? (
+                    <div className="invalid-tooltip" id="tooltipConfirmPassword">{formik.errors.confirmPassword}</div>
+                  ) : null}
                   <label className="form-label" htmlFor="confirmPassword">Подтвердите пароль</label>
                 </div>
                 <button type="submit" className="w-100 btn btn-outline-primary">Зарегистрироваться</button>
