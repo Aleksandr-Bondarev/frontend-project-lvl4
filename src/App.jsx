8d88fd4bc45897406fe3bb9d263ce02bf308af9a
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-06-18 01:18:16 +0300

    rollbar fix 3

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = (socket) => {
     });
 
   const rollbarConfig = {
-    accessToken: '36b31a7d1b694b0697eff2678a405410',
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
     captureUncaught: true,
     captureUnhandledRejections: true,
     payload: {
