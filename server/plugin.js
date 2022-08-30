e204a618761145de0430913129946e8fc29b127c
Aleksandr Bondarev <alexbondarev1331@mail.ru>
2022-08-30 17:28:56 +0300

    fixed server code

--- server/plugin.js
+++ server/plugin.js
@@ -18,14 +18,14 @@ const __filename = fileURLToPath(import.meta.url);
 // eslint-disable-next-line no-underscore-dangle
 const __dirname = path.dirname(__filename);
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 const appPath = path.join(__dirname, '..');
-const isDevelopment = !isProduction;
+// const isDevelopment = !isProduction;
 
 const setUpViews = (app) => {
-  const devHost = 'http://localhost:8090';
+  // const devHost = 'http://localhost:8090';
   // const domain = isDevelopment ? devHost : '';
-  const domain = "";
+  const domain = '';
   app.register(pointOfView, {
     engine: {
       pug: Pug,
